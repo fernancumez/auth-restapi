b94a01a1864c9d893bca14edbca12e35763f12ae
fernancumez <fernancumez@gmail.com>
2021-03-18 13:04:24 -0600

    updated env variables

--- src/config/development.ts
+++ src/config/development.ts
@@ -1,12 +1,13 @@
 import dotenv from "dotenv";
+import { IConfig } from "./types";
 const { NODE_ENV } = process.env;
 
 if (NODE_ENV !== "production") dotenv.config();
 
-const devConfig = {
-  PORT: process.env.PORT,
-  NODE_ENV: process.env.NODE_ENV,
-  DATABASE_URL: `${process.env.DATABASE_URL}`,
+const devConfig: IConfig = {
+  PORT: `${process.env.PORT}`,
+  NODE_ENV: `${process.env.NODE_ENV}`,
+  DATABASE_URL: `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.oxbda.mongodb.net/${process.env.DATABASE_NAME}-${process.env.NODE_ENV}?retryWrites=true&w=majority`,
   JWT_SECRET: `${process.env.JWT_SECRET}`,
 };
 export default devConfig;
---
+++ src/config/types/index.ts
@@ -0,0 +1,6 @@
+export interface IConfig {
+  PORT: string;
+  NODE_ENV: string;
+  DATABASE_URL: string;
+  JWT_SECRET: string;
+}
