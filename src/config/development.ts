4a5c8024fcd15cfc07deb994a5666349524fdd84
fernancumez <fernancumez@gmail.com>
2021-03-21 17:15:10 -0600

    database error connection solved

--- src/config/development.ts
+++ src/config/development.ts
@@ -7,7 +7,7 @@ if (NODE_ENV !== "production") dotenv.config();
 const devConfig: IConfig = {
   PORT: `${process.env.PORT}`,
   NODE_ENV: `${process.env.NODE_ENV}`,
-  DATABASE_URL: `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.oxbda.mongodb.net/${process.env.DATABASE_NAME}-${process.env.NODE_ENV}?retryWrites=true&w=majority`,
+  DATABASE_URL: `${process.env.DATABASE_URL}`,
   JWT_SECRET: `${process.env.JWT_SECRET}`,
 };
 export default devConfig;
